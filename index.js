ee5331abb4311cdb402cdfb27dc7bbc56d337ddd
bnkc-commits <infobnkc@gmail.com>
2026-05-16 01:18:01 +0200

    Set up dotenv and error handling in index.js
    Added dotenv configuration and error handling middleware.

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+
 const PORT = process.env.PORT || 3000;
 
+// Middleware
+app.use(express.json());
+
+// Routes
 app.get("/", (req, res) => {
   res.send("Hello from bankadd_smartdesk!");
 });
 
+app.get("/apporteur-app", (req, res) => {
+  res.send("Hello from Apporteur App");
+});
+
+// Gestion des erreurs
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+// Lancement du serveur
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-}); 
-
-app.get('/apporteur-app', (req, res) => {
-  res.send('Hello from Apporteur App');
 });
