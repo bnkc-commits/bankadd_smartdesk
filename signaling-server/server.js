5c0074d681bcab3590c1c99e592267aac6e79755
bnkc-commits <infobnkc@gmail.com>
2026-05-16 00:42:30 +0200

    Update server.js

--- signaling-server/server.js
+++ signaling-server/server.js
@@ -1,32 +1,11 @@
-const WebSocket = require('ws')
+const WebSocket = require('ws');
 
-const PORT = process.env.PORT || 8080
-const wss = new WebSocket.Server({ port: PORT })
+const server = new WebSocket.Server({ port: process.env.PORT || 3000 });
 
-const clients = {}
-
-wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
-    const data = JSON.parse(message)
-
-    if (data.type === 'register') {
-      clients[data.userId] = ws
-      console.log('Connected:', data.userId)
-      return
-    }
-
-    if (data.to && clients[data.to]) {
-      clients[data.to].send(JSON.stringify(data))
-    }
-  })
-
-  ws.on('close', () => {
-    Object.keys(clients).forEach((id) => {
-      if (clients[id] === ws) {
-        delete clients[id]
-      }
-    })
-  })
-})
-
-console.log(`Signal server started on port ${PORT}`)
+server.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('message', (message) => {
+    console.log(`Received: ${message}`);
+    socket.send(`Echo: ${message}`);
+  });
+});
